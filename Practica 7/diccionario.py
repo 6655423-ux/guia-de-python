aedb17c41de51eb2cc1e81bebfc81c36ef6ec13d
6655423-ux <6655423@clases.edu.sv>
2026-06-03 13:56:41 -0600

    Create diccionario.py

---
+++ Practica 7/diccionario.py
@@ -0,0 +1,15 @@
+#lista de elementos llave:valor
+#arreglos asociativos
+
+persona = {"nombre": "rodrigo",
+           "edad": 800,
+           "apellido": "montemayor"}
+
+persona["apodo"] = "ringa tech"
+
+#print(persona.items())
+
+print("dieccion" in persona)
+
+#for key, value in persona.items():
+#    print(f"la llave {key} tiene valor {value}")
