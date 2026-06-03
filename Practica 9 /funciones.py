960bbe92e5613977a50f5b576a4656232007aa4a
6655423-ux <6655423@clases.edu.sv>
2026-06-03 13:58:36 -0600

    Create funciones.py

---
+++ Practica 9 /funciones.py
@@ -0,0 +1,7 @@
+def saludar_y_sumar(nombre, num1, num2=0):
+    print("saludos", nombre)
+    print("resultado de la suma", num1+num2)
+
+saludar_y_sumar("Nicole", 10, 5)
+saludar_y_sumar("Dinora", 2)
+saludar_y_sumar("Cecilia", 800, 5000)
