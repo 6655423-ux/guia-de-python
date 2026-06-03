e3f6ff1280bfe789e02c36a898b656b89e46044f
6655423-ux <6655423@clases.edu.sv>
2026-06-03 13:57:41 -0600

    Create funciones.py

---
+++ Practica 8/funciones.py
@@ -0,0 +1,17 @@
+#pre-definidas print(),type(), range(), input()
+
+def saludar(nombre):
+    print("hola", nombre)
+
+#Celsius a fahrenheit: (c * 1.8) + 32
+def convertir_a_fahrenheit(c):
+    return (c * 1.8) + 32
+
+
+#saludar("nicol")
+#saludar("cecilia")
+#saludar("dinora")
+
+print( convertir_a_fahrenheit(10) )
+print( convertir_a_fahrenheit(30) )
+print( convertir_a_fahrenheit(80) )
