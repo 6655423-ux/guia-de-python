aca60e6d11a7f6e2e2638d0ca63f487faf526df3
6655423-ux <6655423@clases.edu.sv>
2026-06-03 13:54:31 -0600

    Create condiccionales.py

---
+++ Practica 5 /condiccionales.py
@@ -0,0 +1,12 @@
+#Logico Boolean Boleano
+#verdadero / Falso: True / False
+#dato = 5 == 5
+#> < ==
+
+print("Ingresa tu nombre")
+nombre = input()
+
+if nombre == "saul":
+    print("Bienvenido saul")
+else:
+    print("Que nombre tan extraño")
