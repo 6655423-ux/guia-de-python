684d6c4d4d6e97d6cb8c98f2899b3f372f9cf500
6655423-ux <6655423@clases.edu.sv>
2026-06-03 14:02:36 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Soy un comentario
+#strings, cadenas, textos
+#numericos
+nombre = "saul"
+apellido = "montoya"
+edad = 17
+print(nombre, apellido, edad)
+
+#edad = edad /3
+
+print(nombre, apellido, edad)
+
+print(type(edad))
