0a67e3aa3ed347cbf289eedcbf34552e8dc5320c
6655423-ux <6655423@clases.edu.sv>
2026-06-03 14:01:42 -0600

    Create tuplas.py

---
+++ Practica 12/tuplas.py
@@ -0,0 +1,12 @@
+#tuplas my similares a las listas
+#no pueden modificarse
+#se utilizan parentecis en lugar de []
+
+listas = ["rodrigo", "juan"] #constantemente agragando informacion
+tupla = ("rodrigo", "juan")
+
+videojuegos = []
+#nombre, generio, año
+videojuegos.append( ("final fantasy 7", "jrpg", 1997) )
+videojuegos.append( ("focter wilds", "aventura", 2019) )
+print(videojuegos)
