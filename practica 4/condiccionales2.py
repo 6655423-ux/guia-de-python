8d21072096439a490c4216a3dc78dfe2eb356bcf
6655423-ux <6655423@clases.edu.sv>
2026-06-03 13:53:31 -0600

    Create condiccionales2.py

---
+++ practica 4/condiccionales2.py
@@ -0,0 +1,16 @@
+print("Escribe tu nombre:")
+nombre = input()
+#print("Escribe tu edad")
+#edad = int(input())
+
+#elif
+#Operadores logicos
+#and (y) / or (o)
+#and: Todas las expreciones sean true
+#or: Con que una de las expreciones sea true
+#> <  >= <=
+
+if nombre == "saul" or nombre == "montoya":
+    print("me gusta tu nombre")
+else:
+    print("Que nombre tan extraño") 
